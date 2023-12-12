362cd563cd5ef7072ada31d4733947fc77985d9a
webwizardsusa <webwizardsusa@WWU.local>
2023-12-12 16:03:06 +0530

    Spatie Laravel Permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,28 @@ class User extends Authenticatable
     {
         return $this->role->name === $role;
     }
+
+    public function authorizeRoles($roles)
+    {
+      if ($this->hasAnyRole($roles)) {
+        return true;
+      }
+      abort(401, 'This action is unauthorized.');
+    }
+
+    public function hasAnyRole($roles)
+    {
+      if (is_array($roles)) {
+        foreach ($roles as $role) {
+          if ($this->hasRole($role)) {
+            return true;
+          }
+        }
+      } else {
+        if ($this->hasRole($roles)) {
+          return true;
+        }
+      }
+      return false;
+    }
 }
